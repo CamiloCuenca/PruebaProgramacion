add31c312e4bc8c65b5b9d6b21271a7be4ac5c8c
CamiloCuenca <88161476+CamiloCuenca@users.noreply.github.com>
2023-03-30 21:02:07 -0500

    segundo commit: Se agregaron atributos y se creo constructor del objeto

--- PruebaRepositorio/src/prueba/hola.java
+++ PruebaRepositorio/src/prueba/hola.java
@@ -1,10 +1,22 @@
 package prueba;
 
 public class hola {
+	private String nombre;
+	private String edad;
+	private String id;
+	
+	
+
+	public hola(String nombre, String edad, String id) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+		this.id = id;
+	}
+
+
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
 	}
-	
 	
 }
